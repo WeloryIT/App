45d40b94f12b172344afcc347b47bae93b7ca694
vvl1m <54174788+vvl1m@users.noreply.github.com>
2020-04-14 16:56:03 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="header">
+        <div class="blur">
+          <img
+            src="https://sun3-13.userapi.com/fnd5Pm12GycpUycUoaNbQD0-N1fWM0kI2NxeRw/i6NMolRJ98Y.jpg"
+            class="pizdec"
+          />
+          <h1>Самое полезное приложение студии WeloryIT</h1>
+          <h2>Пока здесь ничего нет, но мы работает над этим</h2>
+          <a href="https://vk.com/weloryit">
+            <img src="https://i.imgur.com/49zRdsK.png" class="vk" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/stas/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <iframe
+        width="853"
+        height="480"
+        src="https://www.youtube.com/embed/Xtb6TMTJdS4"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
+      <a href="../"> Гори в аду)</a>
+    </div>
+  );
+}
