b06558d5135658c6288049accd956ce7f59ddda0
vvl1m <54174788+welorylimano@users.noreply.github.com>
2020-04-16 01:29:05 +0500

    Redesign

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ export default function App() {
     <div className="App">
       <div class="header">
         <div class="blur">
-          <img
-            src="https://sun3-13.userapi.com/fnd5Pm12GycpUycUoaNbQD0-N1fWM0kI2NxeRw/i6NMolRJ98Y.jpg"
-            class="pizdec"
-          />
-          <h1>Самое полезное приложение студии WeloryIT</h1>
-          <h2>Пока здесь ничего нет, но мы работает над этим</h2>
-          <a href="https://vk.com/weloryit">
-            <img src="https://i.imgur.com/49zRdsK.png" class="vk" />
-          </a>
+          <h1 class="h1">Самое полезное приложение студии WeloryIT</h1>
+          <p class="p">Пока здесь ничего нет, но мы работает над этим</p>
+          <div class="links">
+            <a href="https://vk.com/weloryit">
+              <img src="https://i.imgur.com/49zRdsK.png" class="vk" />
+            </a>
+            <a href="https://github.com/weloryit">
+              <img src="https://i.imgur.com/qWhslZ0.png" class="github" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
